9514fad062ba92b4aca74cf8b88a7cc35f3d6f3b
EddieCarrion.github.io <ecar25L@gmail.com>
2023-04-12 19:11:50 -0700

    progress

--- treasure-hunt-eddie/src/App.js
+++ treasure-hunt-eddie/src/App.js
@@ -1,14 +1,21 @@
 import React from "react"
-
+import Board from "./Components/board"
+import "./App.css"
 
 const App = () => {
+  const values = ["?","?","?","?","?","?","?","?","?"]
+  const tiles = values.map((value,index)=>(
+    <Board key={index} value={value}/>
+  ))
   return (
     <>
       <h1>Hello World</h1>
+     <div> {tiles} 
+     
+     </div>
     </>
     
   )
 }
 
-export default App;
-1234
+export default App
---
+++ treasure-hunt-eddie/src/Components/board.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const board = ({value}) => {
+    return (
+        
+            <div className="tiles">
+                {value}
+            </div>
+            
+        
+    )
+}
+
+export default board
