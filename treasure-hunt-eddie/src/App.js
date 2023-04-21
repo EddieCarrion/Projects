7e60dc60f3e62afcbb187eed2dead8bb42dad2d1
EddieCarrion.github.io <ecar25L@gmail.com>
2023-04-21 07:05:02 -0700

    progress

--- treasure-hunt-eddie/src/App.js
+++ treasure-hunt-eddie/src/App.js
@@ -6,7 +6,10 @@ const App = () => {
   
   const [values,setValues] = useState(Array(9).fill("?"))
   const [treasureIndex, setTreasureIndex] = useState(null)
-  const [bombIndex, setbombIndex] = useState(null)
+  const [bombIndex, setBombIndex] = useState(null)
+  const [clickCount, setClickCount] = useState(5)
+  const [currentValue, setCurrentValue] = useState("")
+  const [showMessage, setShowMessage] = useState(false)
   const calculateIndex = () => {
     const newTreasureIndex = Math.floor(Math.random()*9)
     setTreasureIndex(newTreasureIndex)
@@ -14,33 +17,69 @@ const App = () => {
     do {
       newBombIndex = Math.floor(Math.random()*9)
     } while(newBombIndex===newTreasureIndex)
-    setbombIndex(newBombIndex)
+    setBombIndex(newBombIndex)
   }
   useState(()=>{
     calculateIndex()
   },[])
   const displayFunction = (index) => {
-    const newValue = values.slice()
-    let trueValue
-    if (index === treasureIndex){
-      trueValue = "💎"
-    } else if (index === bombIndex){
-      trueValue = "💣"
-    } else {
-      trueValue = "🌲"
+    if (clickCount > 0){
+      const newValue = values.slice()
+      let trueValue
+      if (index === treasureIndex){
+        trueValue = "💎"
+      } else if (index === bombIndex){
+        trueValue = "💣"
+      } else {
+        trueValue = "🌲"
+      }
+      newValue[index] = trueValue
+      const currentValue = newValue[index]
+      setValues(newValue)
+      if (currentValue==="💣"){
+        setClickCount(0)
+        setCurrentValue("💣")
+        setShowMessage(true)
+      } else if (currentValue==="💎"){
+        setClickCount(0)
+        setCurrentValue("💎")
+        setShowMessage(true)
+      } else {
+        setClickCount(clickCount - 1)
+      }
+    
+     
     }
-    newValue[index] = trueValue
-    setValues(newValue)
+    
+    
   }
- 
+  const toggleMessage = () => {
+    setShowMessage(!showMessage)
+  }
+  const reloadPage = () => {
+    toggleMessage() 
+    window.location.reload()
+  }
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Treasure Hunt Game</h1>
+      <div className="clickCount">
+        attempts remaining
+        <div>{clickCount}</div> 
+
+
+      </div>
      <div className="container"> 
       {values.map((value,index)=>(
          <Board key={index} value={value} displayFunction={()=>displayFunction(index)}/>
       ))}
-     
+     {showMessage && (
+      <div className="message">
+        {currentValue==="💣" && <p>oh no you lost!</p>}
+        {currentValue==="💎" && <p>Yes! You win!</p>}
+        <button onClick={reloadPage}>Would You like to play again?</button>
+        </div>
+     )}
      </div>
     </>
     
