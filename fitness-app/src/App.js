ee81d6c53aba0146a2a0483bd8813116fa0b16b9
EddieCarrion.github.io <ecar25L@gmail.com>
2023-05-23 19:14:17 -0700

    progress

---
+++ fitness-app/src/App.js
@@ -0,0 +1,21 @@
+import react from "react"
+import './App.css';
+// import FitnessHome from '../pages/FitnessHome'
+import Cards from './Components/Cards.js'
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Hello</h1>
+   
+
+    <Cards />
+
+
+    </div>
+  )
+
+}
+
+export default App;
---
+++ fitness-app/src/Components/Cards.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+
+const Cards = () => {
+    return (
+         <div className="card">
+            <img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfGkNGWh68HMC7YWVcYbiDnEBo6Og8YPhQ1Q&usqp=CAU" alt="Oops!"/>
+            <div className="container">
+                <h4><b>Leg Exercises</b></h4>
+                
+            </div>
+         </div>
+    )
+}
+
+export default Cards;
